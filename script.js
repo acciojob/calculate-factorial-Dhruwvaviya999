ac32df33e8a65342a989e2da14d5aa8ac862d638
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-07-23 14:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+// Prompt the user for a number
+let input = prompt("Enter a number to find its factorial:");
+
+// Convert the input to a number
+let num = parseInt(input);
+
+// Initialize factorial
+let factorial = 1;
+
+// Edge case: If num is 0 or 1, factorial is 1
+if (num === 0 || num === 1) {
+  factorial = 1;
+} else {
+  for (let i = 2; i <= num; i++) {
+    factorial *= i;
+  }
+}
+
+// Show the result
+alert("The factorial of " + num + " is " + factorial);
